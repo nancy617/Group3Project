868a123956fda54df99364f410979877be4bb7c8
bharani16592 <bkalagotla@oakland.edu>
2022-04-15 16:41:30 -0400

    updated enviroment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiBaseURL:"backendaws-env.eba-fdddft5p.us-east-2.elasticbeanstalk.com"
+  apiBaseURL:"http://backendaws-env.eba-fdddft5p.us-east-2.elasticbeanstalk.com"
+  /*apiBaseURL: "http://localhost:8080"*/
 };
